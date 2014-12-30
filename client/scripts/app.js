6287a6fd923e580ca03e7da5882fdcb79b4c91ff
samsep <saminsepasi@gmail.com>
2014-12-30 11:54:03 -0800

    Basic functionality of chatterbox completed.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,36 +1,23 @@
 // YOUR CODE HERE:
 $(document).ready(function() {
 
-var app = {};
-var timeStamp = "2012-10-07T17:24:40.668Z";
-app.init = function () {
-  this.server = 'https://api.parse.com/1/classes/chatterbox'
-};
+  var app = {};
+  var timeStamp = "2012-10-07T17:24:40.668Z";
+  var roomName = 'default';
+  app.init = function () {
+    this.server = 'https://api.parse.com/1/classes/chatterbox'
+  };
+  var friends = [];
 
-// var entityMap = {
-//     "&": "&amp;",
-//     "<": "&lt;",
-//     ">": "&gt;",
-//     '"': '&quot;',
-//     "'": '&#39;',
-//     "/": '&#x2F;'
-//   };
-
-//   function escapeHtml(string) {
-//     return String(string).replace(/[&<>"'\/]/g, function (s) {
-//       return entityMap[s];
-//     });
-//   }
 
 app.clearMessages = function() {
   $('#chats').empty();
 }
 
-//this breaks the addMessage spec.
 app.addMessage = function(message) {
   var $chat = $('<li></li>');
-      $chat.text(message.username + ": " + message.roomname + ": " + message.text);
-      $('#chats').prepend($chat);
+  $chat.text(message.username + ": " + message.roomname + ": " + message.text);
+  $('#chats').prepend($chat);
 }
 
 app.addRoom = function(roomName) {
@@ -55,21 +42,30 @@ app.send = function(message) {
 };
 
 var displayMessages = function (response) {
-    // console.log(response);
+
     response.results.forEach(function(chat) {
-      // var $chat = $('<li>' + escapeHtml(chat.username) + ": " + escapeHtml(chat.roomname) + ": " + escapeHtml(chat.text) + '</li>')
-      // $('.main ul').append($chat);
+
       if(chat.createdAt > timeStamp) {
         timeStamp = chat.createdAt;
         var $chat = $('<li></li>');
-        $chat.text(chat.username + ": " + chat.roomname + ": " + chat.text);
+        var $username = $('<a href=#></a>');
+        $username.addClass(chat.username);
+        $username.text(chat.username);
+        $chat.text(chat.roomname + ": " + chat.text);
+        $chat.prepend($username);
+        $chat.addClass(chat.roomname);
         $('.main ul').prepend($chat);
-     }
+        var $room = $('<span></span>');
+        $room.text(chat.roomname);
+        if(!$('select').find('option:contains('+ $room.text() + ')').length > 0 && $room.text() !== "") {
+          $('select').append('<option value=' + $room.text() + '>' + $room.text() + '</option>');
+        }
+      }
     });
-}
+  }
 
-app.fetch = function(fetchCb) {
-  $.ajax({
+  app.fetch = function(fetchCb) {
+    $.ajax({
     // always use this url
     url: 'https://api.parse.com/1/classes/chatterbox',
     // alex says we can put something at end of url to filter
@@ -82,28 +78,70 @@ app.fetch = function(fetchCb) {
     },
     error: function (response) {
       // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to send message');
+      console.error('chatterbox: Failed to get message');
     }
   });
-};
-app.fetch(displayMessages);
+  };
+  app.fetch(displayMessages);
 
 
-setInterval(function() {
+  setInterval(function() {
     app.fetch(displayMessages);
-}, 1000);
+  }, 800);
 
-app.handleSubmit = function() {
-}
-  $('body').on("click", "button", function() {
-  var usename = window.location.search.split("=").pop();
-  var message = $('input').text();
-  var rmname = 'default';
+  app.handleSubmit = function() {
+  };
+
+  $('body').on("click", "button.sendMsg", function() {
+    var usename = window.location.search.split("=").pop();
+    var message = $('input.sendMsg').val();
+
+    var rmname = $('select option:selected').text();
+
     event.preventDefault();
     console.log('clicked');
     app.send({username: usename, text: message, roomname: rmname});
+    $('input').val('');
   });
 
+  $('body').on('click', 'button.room', function(){
+    var roomName = $('input.room').val();
+
+    $('select').append('<option value=' + roomName + '>' + roomName + '</option>');
+    $('select').val(roomName);
+    $('select').change();
+  });
+
+  $('body').on('change', 'select', function(){
+
+    filterByRoom();
+    setInterval(filterByRoom, 1000);
+
+  });
+
+  var filterByRoom = function(){
+    var selectedRoom = $('select option:selected').text();
+
+    $('li:not('+selectedRoom+')').hide();
+    $('li.'+selectedRoom).show();
+  }
+
+  $('body').on('click', 'a', function(){
+
+    var friendClass = $(this).attr('class');
+    friends.push(friendClass);
+    setInterval(boldFriends, 100);
+  });
+
+var boldFriends = function () {
+
+  $('a').each(function(ind, friend){
+    console.log(friend);
+    if (friends.indexOf($(friend).attr('class')) !== -1) {
+      $(friend).closest('li').css("font-weight", "bold");
+    }
+  });
+}
 
 })
 
